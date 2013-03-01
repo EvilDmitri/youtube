9fb01c2f3fcf369e538ddb1acaa7fd4a55e61903
EvilDmitri <dmitribrach@gmail.com>
2013-03-01 12:48:58 +0200

    done

--- search_by_topic.py
+++ search_by_topic.py
@@ -8,8 +8,6 @@ import urllib
 
 # Set DEVELOPER_KEY to the "API key" value from the "Access" tab of the
 # Google APIs Console http://code.google.com/apis/console#access
-# Please ensure that you have enabled the Youtube Data API and Freebase API
-# for your project
 DEVELOPER_KEY = 'AIzaSyBX-57s6vyT-6sz5xKJerB-mawR7AhPNZM'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
@@ -20,3 +18,55 @@ def get_topic_id(options):
     freebase_url = FREEBASE_SEARCH_URL % urllib.urlencode(freebase_params)
     freebase_response = json.loads(urllib.urlopen(freebase_url).read())
 
+    if len(freebase_response["result"]) == 0:
+        exit("No matching terms were found in Freebase")
+
+    mids = []
+    index = 1
+    print "The following topics were found:"
+    for result in freebase_response["result"]:
+        mids.append(result["mid"])
+        print "  %2d. %s (%s)" % (index, result.get("name", "Unknown"),
+                result.get("notable", {}).get("name", "Unknown"))
+        index += 1
+
+    mid = None
+    while mid is None:
+        index = raw_input("Enter a topic number to find realated YouTube %ss: " % options.type)
+        try:
+            mid = mids[int(index) - 1]
+        except ValueError:
+            pass
+    return mid
+
+def youtube_search(mid, options):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
+    search_response = youtube.search().list(
+        # The q= parameter is currently required, but in the future jaust a topicId
+        # will be sufficient.
+        q=options.query,
+        topicId=mid,
+        type=options.type,
+        part="id,snippet",
+        maxResults=options.maxResults
+    ).execute()
+
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            print "%s (%s)" % (search_result["snippet"]["title"], search_result["id"]["videoId"])
+        elif search_result["id"]["kind"] == "youtube#channel":
+            print "%s (%s)" % (search_result["snuppet"]["title"], search_result["id"]["channelId"])
+        elif search_result["id"]["kind"] == "youtube#playlist":
+            print "%s (%s)" % (search_result["snuppet"]["title"], search_result["id"]["playlistlId"])
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("--query", dest="query", help="Freebase search term", default="Google")
+    parser.add_option("--max-result", dest="maxResults", help="Max YouTube results", default=25)
+    parser.add_option("--type", dest="type", help="YouTube result type: video, playlist, or channel", default="channel")
+    (options, args) = parser.parse_args()
+
+    mid = get_topic_id(options)
+    youtube_search(mid, options)
